1053c40458792cc0eed8b676e17ce14f2ba6d72f
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2020-12-06 12:16:05 +0000

    Issue 6 fixed

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -2,5 +2,12 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-        pass
+class TestCardGame(unittest.TestCase):
+    def setUp(self):
+        self.ace = Card('hearts',1)
+        self.card2 = Card('hearts', 5)
+        self.card_game = CardGame()
+
+    def test_check_for_ace(self):
+        self.assertTrue(self.card_game(self.ace))
+        
