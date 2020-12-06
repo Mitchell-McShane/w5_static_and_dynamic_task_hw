27f6f0e4383190c81b68f41f41e3dbda2c98e909
Mitchell McShane <mitchell.mcshane1987@gmail.com>
2020-12-06 13:04:36 +0000

    issue 7 fixed added new code to test file

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -4,10 +4,19 @@ from src.card_game import CardGame
 
 class TestCardGame(unittest.TestCase):
     def setUp(self):
-        self.ace = Card('hearts',1)
+        self.ace = Card('hearts', 1)
         self.card2 = Card('hearts', 5)
-        self.card_game = CardGame
+        self.card3 = Card('hearts', 6)
+        self.deck_of_cards = [self.ace, self.card2, self.card3]
+        self.card_game = CardGame()
 
     def test_check_for_ace(self):
         self.assertTrue(self.card_game.check_for_ace(self.ace))
-        
+        self.assertFalse(self.card_game.check_for_ace(self.card2))
+
+    def test_highest_card(self):
+        self.hightest_card = self.card_game.highest_card(self.card2, self.card3)
+        self.assertEqual(self.hightest_card.value, 6)
+
+        self.hightest_card = self.card_game.highest_card(self.card3, self.card2)
+        self.assertEqual(self.hightest_card.value, 6)
